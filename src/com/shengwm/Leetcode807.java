a193726de4d42cb6a0bfd5c34c22cd6de26e338d
shengwenming <shengwenming89@163.com>
2018-05-28 17:32:04 +0800

    maxIncresaseKeepingSkyLine

---
+++ src/com/shengwm/Leetcode807.java
@@ -0,0 +1,51 @@
+package com.shengwm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 保持城市天际线
+ *
+ * @author shengwm
+ * @create 2018-05-25 14:21
+ **/
+public class Leetcode807 {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("shengwm", "盛文明");
+        map.put("company", "上海 鼎视");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String key : map.keySet()) {
+            stringBuilder.append(String.format("%s=%s", key, map.get(key)));
+        }
+        System.out.println(stringBuilder.toString());
+
+
+        int[][] grid = {{59,88,44},{3,18,38},{21,26,51}};
+        int n = maxIncresaseKeepingSkyLine(grid);
+
+        System.out.println(n);
+
+    }
+
+    public static  int maxIncresaseKeepingSkyLine(int[][] grid) {
+        int N = grid.length;
+        int[] rowmaxes = new int[N];
+        int[] colmaxes = new int[N];
+        for (int r = 0; r < N; ++r) {
+            for (int c = 0; c < N; ++c) {
+                rowmaxes[r] = Math.max(rowmaxes[r], grid[r][c]);
+                colmaxes[c] = Math.max(colmaxes[c], grid[r][c]);
+            }
+        }
+
+        int ans = 0;
+        for (int r = 0; r < N; ++r) {
+            for (int c = 0; c < N; ++c) {
+                ans += Math.min(rowmaxes[r], colmaxes[c]) - grid[r][c];
+            }
+        }
+
+        return ans;
+    }
+}
